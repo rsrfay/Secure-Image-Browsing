cf6115c146db75132e0af7ca86aa8a9f4562235d
Russarin <russarin.eai@student.mahidol.edu>
2024-04-02 22:24:40 +0700

    edit bg

--- homepage.py
+++ homepage.py
@@ -78,24 +78,22 @@ def encrypt_image(img_bytes):
     void = expected_size - len(iv) - padded_size
     iv_ciphertext_void = iv + ciphertext + bytes([0] * void)
     
-    # print("iv_ciphertext_void size:", len(iv_ciphertext_void))
-    # print("Expected size:", expected_size)
+    print("iv_ciphertext_void size:", len(iv_ciphertext_void))
+    print("Expected size:", expected_size)
 
-    reshaped_array = np.frombuffer(iv_ciphertext_void, dtype=np.uint8).reshape(row_orig + 1, column_orig, depth_orig)
-    _, encoded_img = cv2.imencode('.jpg', reshaped_array)
-    # reshaped_array = np.frombuffer(iv_ciphertext_void, dtype=np.uint8).reshape(row_orig, column_orig, depth_orig)
+    reshaped_array = np.frombuffer(iv_ciphertext_void, dtype=np.uint8).reshape(row_orig, column_orig, depth_orig)
 
-    # # Encode the reshaped array to a JPEG image
-    # success, encoded_img = cv2.imencode('.jpg', reshaped_array)
+    # Encode the reshaped array to a JPEG image
+    success, encoded_img = cv2.imencode('.jpg', reshaped_array)
 
-    # if not success:
-    #     return b'Encoding failed'
+    if not success:
+        return b'Encoding failed'
 
     # Convert encoded image data to bytes
     encrypted_img = encoded_img.tobytes()
 
-    # # Concatenate IV and ciphertext
-    # encrypted_img = iv + ciphertext
+    # Concatenate IV and ciphertext
+    encrypted_img = iv + ciphertext
 
     return encrypted_img, key
 
