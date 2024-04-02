8d598b5589dc40301ab88d39e36a6e78df9dce12
Chaninan Phetpangun <chaninan.phe@student.mahidol.ac.th>
2024-04-02 20:23:26 +0700

    Create imgb.sql

---
+++ imgb.sql
@@ -0,0 +1,18 @@
+drop database if exists imgbDB;
+
+create database if not exists imgbDB
+	default character set utf8mb4
+	default collate utf8mb4_0900_ai_ci;
+
+use imgbDB;
+
+create table if not exists images(
+imageID int auto_increment primary key,
+Img LONGBLOB,
+encryptedImgcustomers LONGBLOB,
+keyImg BLOB,
+decryptedImg LONGBLOB
+);
+
+drop table images;
+
